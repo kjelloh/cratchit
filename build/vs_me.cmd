f06808caace9f488350b99434d772710cce922b0
kjelloh <kjell-olov.hogdahl@itfied.se>
2016-08-01 14:37:26 +0200

    Restructured source architecture to build/src idiom + Moved SIE source to sie folder. + Fixed error in vs_me DOS command script (Linux rm-command does not work on DOS)

--- build/vs_me.cmd
+++ build/vs_me.cmd
@@ -1,13 +1,10 @@
-REM @ECHO OFF
 REM Use Cmake to generate Visual Studio (VS) build environment
 SET BUILD_DIR=build_vs
 if exist %BUILD_DIR% (
-	rm /sq %BUILD_DIR%
+	rmdir /s/q %BUILD_DIR%
 )
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 REM use Cmake to generate default build environment (on windows it is Visual Studio)
 cmake ..
 cd ..
-ECHO Done.
-PAUSE
