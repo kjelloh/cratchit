b69232edd585c833e95c1eec05512114d333f31b
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-04-07 16:46:49 +0200

    Added conversion rom SIE-file code point 437 to UTF-8 encoding (the reverse not yet implemented)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <locale>
 #include <string>
 #include <sstream>
 #include <queue>
@@ -1212,7 +1213,82 @@ std::vector<std::string> help_for(PromptState const& prompt_state) {
 }
 
 namespace SIE {
+
+	extern std::map<char,char16_t> cp435ToUnicodeMap;
+
+	char16_t cp435ToUnicode(char ch435) {
+		return cp435ToUnicodeMap[ch435];
+	}
+
+	using UTF16String = std::wstring;
+
+	UTF16String cp435ToUnicode(std::string s435) {
+		UTF16String result{};
+		std::transform(s435.begin(),s435.end(),std::back_inserter(result),[](char ch){
+			return cp435ToUnicode(ch);
+		});
+		return result;
+	}
+
+	namespace UTF8 {
+    // Unicode constants
+    // Leading (high) surrogates: 0xd800 - 0xdbff
+    // Trailing (low) surrogates: 0xdc00 - 0xdfff
+    const char32_t LEAD_SURROGATE_MIN  = 0x0000d800;
+    const char32_t LEAD_SURROGATE_MAX  = 0x0000dbff;
+    const char32_t TRAIL_SURROGATE_MIN = 0x0000dc00;
+    const char32_t TRAIL_SURROGATE_MAX = 0x0000dfff;
+
+    // Maximum valid value for a Unicode code point
+    const char32_t CODE_POINT_MAX      = 0x0010ffff;
+
+		inline bool is_surrogate(char32_t cp) {
+					return (cp >= LEAD_SURROGATE_MIN && cp <= TRAIL_SURROGATE_MAX);
+		}
+
+		bool is_valid_unicode(char32_t cp) {
+			return (cp <= CODE_POINT_MAX && !is_surrogate(cp));
+		}
+
+		struct Stream {
+			std::ostream& os;
+		};
+
+		UTF8::Stream& operator<<(UTF8::Stream& os,char32_t cp) {
+			// Thanks to https://sourceforge.net/p/utfcpp/code/HEAD/tree/v3_0/src/utf8.h
+			if (!is_valid_unicode(cp)) {
+				os.os << '?';
+			}
+			else if (cp < 0x80)                        // one octet
+					os.os << static_cast<char>(cp);
+			else if (cp < 0x800) {                // two octets
+					os.os << static_cast<char>((cp >> 6)            | 0xc0);
+					os.os << static_cast<char>((cp & 0x3f)          | 0x80);
+			}
+			else if (cp < 0x10000) {              // three octets
+					os.os << static_cast<char>((cp >> 12)           | 0xe0);
+					os.os << static_cast<char>(((cp >> 6) & 0x3f)   | 0x80);
+					os.os << static_cast<char>((cp & 0x3f)          | 0x80);
+			}
+			else {                                // four octets
+					os.os << static_cast<char>((cp >> 18)           | 0xf0);
+					os.os << static_cast<char>(((cp >> 12) & 0x3f)  | 0x80);
+					os.os << static_cast<char>(((cp >> 6) & 0x3f)   | 0x80);
+					os.os << static_cast<char>((cp & 0x3f)          | 0x80);
+			}
+			return os;
+		}
+
+		std::string unicode_to_utf8(UTF16String const& s) {
+			std::ostringstream os{};
+			UTF8::Stream utf8_os{os};
+			for (auto cp : s) utf8_os << cp;
+			return os.str();
+		}
+	}
+
 	using Stream = std::istream;
+
 	struct Tag {
 		std::string const expected;
 	};
@@ -1354,6 +1430,12 @@ namespace SIE {
 		SIE::Konto konto{};
 		auto pos = in.tellg();
 		if (in >> Tag{konto_tag} >> konto.account_no >> std::quoted(konto.name)) {
+
+			// Convert from code point 437 to utf-8
+			auto s437 = konto.name;
+			auto sUnicode = cp435ToUnicode(s437);
+			konto.name = UTF8::unicode_to_utf8(sUnicode);
+
 			result = konto;
 			pos = in.tellg(); // accept new stream position
 		}
@@ -1385,6 +1467,14 @@ namespace SIE {
 		optional_Text_parser optional_transtext_parser{trans.transtext};
 		if (in >> Tag{trans_tag} >> trans.account_no >> trans.object_list >> trans.amount >> optional_transdate_parser >> optional_transtext_parser >> scraps) {
 			// std::cout << trans_tag << trans.account_no << " " << trans.amount;
+
+			if (trans.transtext) {
+				// Convert from code point 437 to utf-8
+				auto s437 = *trans.transtext;
+				auto sUnicode = cp435ToUnicode(s437);
+				trans.transtext = UTF8::unicode_to_utf8(sUnicode);
+			}
+
 			result = trans;
 			pos = in.tellg(); // accept new stream position
 		}
@@ -1416,30 +1506,35 @@ namespace SIE {
 		if (in >> Tag{"#VER"} >> ver.series >> ver.verno >> verdate_parser >> std::quoted(ver.vertext) >> scraps >> scraps) {
 			if (true) {
 				// std::cout << "\nVer: " << ver.series << " " << ver.verno << " " << ver.vertext;
-				while (true) {
-					if (auto entry = parse_TRANS(in,"#TRANS")) {
-						// std::cout << "\nTRANS :)";	
-						ver.transactions.push_back(std::get<Trans>(*entry));					
-					}
-					else if (auto entry = parse_TRANS(in,"#BTRANS")) {
-						// Ignore
-						// std::cout << " Ignored";
-					}
-					else if (auto entry = parse_TRANS(in,"#RTRANS")) {
-						// Ignore
-						// std::cout << " Ignored";
-					}
-					else if (auto entry = parse_Tag(in,"}")) {
-						break;
-					}
-					else {
-						std::cerr << "\nERROR - Unexpected input while parsing #VER SIE entry";
-						break;
-					}
-				}
-				result = ver;
-				pos = in.tellg(); // accept new stream position
 			}
+			// Convert from code point 437 to utf-8
+			auto s437 = ver.vertext;
+			auto sUnicode = cp435ToUnicode(s437);
+			ver.vertext = UTF8::unicode_to_utf8(sUnicode);
+
+			while (true) {
+				if (auto entry = parse_TRANS(in,"#TRANS")) {
+					// std::cout << "\nTRANS :)";	
+					ver.transactions.push_back(std::get<Trans>(*entry));					
+				}
+				else if (auto entry = parse_TRANS(in,"#BTRANS")) {
+					// Ignore
+					// std::cout << " Ignored";
+				}
+				else if (auto entry = parse_TRANS(in,"#RTRANS")) {
+					// Ignore
+					// std::cout << " Ignored";
+				}
+				else if (auto entry = parse_Tag(in,"}")) {
+					break;
+				}
+				else {
+					std::cerr << "\nERROR - Unexpected input while parsing #VER SIE entry";
+					break;
+				}
+			}
+			result = ver;
+			pos = in.tellg(); // accept new stream position
 		}
 		in.seekg(pos); // Reset position in case of failed parse
 		in.clear(); // Reset failbit to allow try for other parse
@@ -2234,15 +2329,288 @@ private:
 
 int main(int argc, char *argv[])
 {
-    std::string sPATH{std::getenv("PATH")};
-    // std::cout << "\nPATH=" << sPATH;
-    std::string command{};
-    for (int i=1;i<argc;i++) command+= std::string{argv[i]} + " ";
-    auto current_path = std::filesystem::current_path();
-    auto environment_file_path = current_path / "cratchit.env";
-    REPL repl{environment_file_path};
-    repl.run(command);
-    // std::cout << "\nBye for now :)";
-    std::cout << std::endl;
-    return 0;
+	if (false) {
+		// Log current locale and test charachter encoding.
+		// TODO: Activate to adjust for cross platform handling 
+			std::wcout << "\nDeafult (current) locale setting is " << std::locale().name().c_str();
+			std::string sHello{"Hallå Åland! Ömsom ödmjuk. Ärligt äkta."}; // This source file expected to be in UTF-8
+			std::cout << "\ncout:" << sHello; // macOS handles UTF-8 ok (how about other platforms?)
+			for (auto const& ch : sHello) {
+				std::cout << "\n" << ch << " " << std::hex << static_cast<int>(ch) << std::dec; // check stream and console encoding, std::locale behaviour
+			}
+			std::string sPATH{std::getenv("PATH")};
+			// std::cout << "\nPATH=" << sPATH;
+	}
+	std::string command{};
+	for (int i=1;i<argc;i++) command+= std::string{argv[i]} + " ";
+	auto current_path = std::filesystem::current_path();
+	auto environment_file_path = current_path / "cratchit.env";
+	REPL repl{environment_file_path};
+	repl.run(command);
+	// std::cout << "\nBye for now :)";
+	std::cout << std::endl;
+	return 0;
 }
+
+namespace SIE {
+
+	// from http://www.unicode.org/Public/MAPPINGS/VENDORS/MICSFT/PC/CP437.TXT
+	std::map<char,char16_t> cp435ToUnicodeMap{
+	{0x0,0x0}
+	,{0x1,0x1}
+	,{0x2,0x2}
+	,{0x3,0x3}
+	,{0x4,0x4}
+	,{0x5,0x5}
+	,{0x6,0x6}
+	,{0x7,0x7}
+	,{0x8,0x8}
+	,{0x9,0x9}
+	,{0xA,0xA}
+	,{0xB,0xB}
+	,{0xC,0xC}
+	,{0xD,0xD}
+	,{0xE,0xE}
+	,{0xF,0xF}
+	,{0x10,0x10}
+	,{0x11,0x11}
+	,{0x12,0x12}
+	,{0x13,0x13}
+	,{0x14,0x14}
+	,{0x15,0x15}
+	,{0x16,0x16}
+	,{0x17,0x17}
+	,{0x18,0x18}
+	,{0x19,0x19}
+	,{0x1A,0x1A}
+	,{0x1B,0x1B}
+	,{0x1C,0x1C}
+	,{0x1D,0x1D}
+	,{0x1E,0x1E}
+	,{0x1F,0x1F}
+	,{0x20,0x20}
+	,{0x21,0x21}
+	,{0x22,0x22}
+	,{0x23,0x23}
+	,{0x24,0x24}
+	,{0x25,0x25}
+	,{0x26,0x26}
+	,{0x27,0x27}
+	,{0x28,0x28}
+	,{0x29,0x29}
+	,{0x2A,0x2A}
+	,{0x2B,0x2B}
+	,{0x2C,0x2C}
+	,{0x2D,0x2D}
+	,{0x2E,0x2E}
+	,{0x2F,0x2F}
+	,{0x30,0x30}
+	,{0x31,0x31}
+	,{0x32,0x32}
+	,{0x33,0x33}
+	,{0x34,0x34}
+	,{0x35,0x35}
+	,{0x36,0x36}
+	,{0x37,0x37}
+	,{0x38,0x38}
+	,{0x39,0x39}
+	,{0x3A,0x3A}
+	,{0x3B,0x3B}
+	,{0x3C,0x3C}
+	,{0x3D,0x3D}
+	,{0x3E,0x3E}
+	,{0x3F,0x3F}
+	,{0x40,0x40}
+	,{0x41,0x41}
+	,{0x42,0x42}
+	,{0x43,0x43}
+	,{0x44,0x44}
+	,{0x45,0x45}
+	,{0x46,0x46}
+	,{0x47,0x47}
+	,{0x48,0x48}
+	,{0x49,0x49}
+	,{0x4A,0x4A}
+	,{0x4B,0x4B}
+	,{0x4C,0x4C}
+	,{0x4D,0x4D}
+	,{0x4E,0x4E}
+	,{0x4F,0x4F}
+	,{0x50,0x50}
+	,{0x51,0x51}
+	,{0x52,0x52}
+	,{0x53,0x53}
+	,{0x54,0x54}
+	,{0x55,0x55}
+	,{0x56,0x56}
+	,{0x57,0x57}
+	,{0x58,0x58}
+	,{0x59,0x59}
+	,{0x5A,0x5A}
+	,{0x5B,0x5B}
+	,{0x5C,0x5C}
+	,{0x5D,0x5D}
+	,{0x5E,0x5E}
+	,{0x5F,0x5F}
+	,{0x60,0x60}
+	,{0x61,0x61}
+	,{0x62,0x62}
+	,{0x63,0x63}
+	,{0x64,0x64}
+	,{0x65,0x65}
+	,{0x66,0x66}
+	,{0x67,0x67}
+	,{0x68,0x68}
+	,{0x69,0x69}
+	,{0x6A,0x6A}
+	,{0x6B,0x6B}
+	,{0x6C,0x6C}
+	,{0x6D,0x6D}
+	,{0x6E,0x6E}
+	,{0x6F,0x6F}
+	,{0x70,0x70}
+	,{0x71,0x71}
+	,{0x72,0x72}
+	,{0x73,0x73}
+	,{0x74,0x74}
+	,{0x75,0x75}
+	,{0x76,0x76}
+	,{0x77,0x77}
+	,{0x78,0x78}
+	,{0x79,0x79}
+	,{0x7A,0x7A}
+	,{0x7B,0x7B}
+	,{0x7C,0x7C}
+	,{0x7D,0x7D}
+	,{0x7E,0x7E}
+	,{0x7F,0x7F}
+	,{0x80,0xC7}
+	,{0x81,0xFC}
+	,{0x82,0xE9}
+	,{0x83,0xE2}
+	,{0x84,0xE4}
+	,{0x85,0xE0}
+	,{0x86,0xE5}
+	,{0x87,0xE7}
+	,{0x88,0xEA}
+	,{0x89,0xEB}
+	,{0x8A,0xE8}
+	,{0x8B,0xEF}
+	,{0x8C,0xEE}
+	,{0x8D,0xEC}
+	,{0x8E,0xC4}
+	,{0x8F,0xC5}
+	,{0x90,0xC9}
+	,{0x91,0xE6}
+	,{0x92,0xC6}
+	,{0x93,0xF4}
+	,{0x94,0xF6}
+	,{0x95,0xF2}
+	,{0x96,0xFB}
+	,{0x97,0xF9}
+	,{0x98,0xFF}
+	,{0x99,0xD6}
+	,{0x9A,0xDC}
+	,{0x9B,0xA2}
+	,{0x9C,0xA3}
+	,{0x9D,0xA5}
+	,{0x9E,0x20A7}
+	,{0x9F,0x192}
+	,{0xA0,0xE1}
+	,{0xA1,0xED}
+	,{0xA2,0xF3}
+	,{0xA3,0xFA}
+	,{0xA4,0xF1}
+	,{0xA5,0xD1}
+	,{0xA6,0xAA}
+	,{0xA7,0xBA}
+	,{0xA8,0xBF}
+	,{0xA9,0x2310}
+	,{0xAA,0xAC}
+	,{0xAB,0xBD}
+	,{0xAC,0xBC}
+	,{0xAD,0xA1}
+	,{0xAE,0xAB}
+	,{0xAF,0xBB}
+	,{0xB0,0x2591}
+	,{0xB1,0x2592}
+	,{0xB2,0x2593}
+	,{0xB3,0x2502}
+	,{0xB4,0x2524}
+	,{0xB5,0x2561}
+	,{0xB6,0x2562}
+	,{0xB7,0x2556}
+	,{0xB8,0x2555}
+	,{0xB9,0x2563}
+	,{0xBA,0x2551}
+	,{0xBB,0x2557}
+	,{0xBC,0x255D}
+	,{0xBD,0x255C}
+	,{0xBE,0x255B}
+	,{0xBF,0x2510}
+	,{0xC0,0x2514}
+	,{0xC1,0x2534}
+	,{0xC2,0x252C}
+	,{0xC3,0x251C}
+	,{0xC4,0x2500}
+	,{0xC5,0x253C}
+	,{0xC6,0x255E}
+	,{0xC7,0x255F}
+	,{0xC8,0x255A}
+	,{0xC9,0x2554}
+	,{0xCA,0x2569}
+	,{0xCB,0x2566}
+	,{0xCC,0x2560}
+	,{0xCD,0x2550}
+	,{0xCE,0x256C}
+	,{0xCF,0x2567}
+	,{0xD0,0x2568}
+	,{0xD1,0x2564}
+	,{0xD2,0x2565}
+	,{0xD3,0x2559}
+	,{0xD4,0x2558}
+	,{0xD5,0x2552}
+	,{0xD6,0x2553}
+	,{0xD7,0x256B}
+	,{0xD8,0x256A}
+	,{0xD9,0x2518}
+	,{0xDA,0x250C}
+	,{0xDB,0x2588}
+	,{0xDC,0x2584}
+	,{0xDD,0x258C}
+	,{0xDE,0x2590}
+	,{0xDF,0x2580}
+	,{0xE0,0x3B1}
+	,{0xE1,0xDF}
+	,{0xE2,0x393}
+	,{0xE3,0x3C0}
+	,{0xE4,0x3A3}
+	,{0xE5,0x3C3}
+	,{0xE6,0xB5}
+	,{0xE7,0x3C4}
+	,{0xE8,0x3A6}
+	,{0xE9,0x398}
+	,{0xEA,0x3A9}
+	,{0xEB,0x3B4}
+	,{0xEC,0x221E}
+	,{0xED,0x3C6}
+	,{0xEE,0x3B5}
+	,{0xEF,0x2229}
+	,{0xF0,0x2261}
+	,{0xF1,0xB1}
+	,{0xF2,0x2265}
+	,{0xF3,0x2264}
+	,{0xF4,0x2320}
+	,{0xF5,0x2321}
+	,{0xF6,0xF7}
+	,{0xF7,0x2248}
+	,{0xF8,0xB0}
+	,{0xF9,0x2219}
+	,{0xFA,0xB7}
+	,{0xFB,0x221A}
+	,{0xFC,0x207F}
+	,{0xFD,0xB2}
+	,{0xFE,0x25A0}
+	,{0xFF,0xA0}
+	};
+} // namespace SIE
