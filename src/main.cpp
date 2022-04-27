0159f1186451b0d244f56eb029ce1967d9add097
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-04-27 23:25:46 +0200

    Minor cleanup of "to uppercase" and "s1 in s2" code

--- src/main.cpp
+++ src/main.cpp
@@ -224,21 +224,40 @@ namespace parse {
     }
 } // namespace parse
 
+auto utf_ignore_to_upper_f = [](char ch) {
+	if (ch <= 0x7F) return static_cast<char>(std::toupper(ch));
+	return ch;
+};
+
+std::string utf_ignore_to_upper(std::string const& s) {
+	std::string result{};
+	std::transform(s.begin(),s.end(),std::back_inserter(result),utf_ignore_to_upper_f);
+	return result;
+}
+
+std::vector<std::string> utf_ignore_to_upper(std::vector<std::string> const& tokens) {
+	std::vector<std::string> result{};
+	auto f = [](std::string s) {return utf_ignore_to_upper(s);};
+	std::transform(tokens.begin(),tokens.end(),std::back_inserter(result),f);
+	return result;
+}
+
 bool strings_share_tokens(std::string const& s1,std::string const& s2) {
 	bool result{false};
-	auto had_heading_words = tokenize::splits(s1);
-	auto je_heading_words = tokenize::splits(s2);
-	for (std::string hadw : had_heading_words) {
-		for (std::string jew : je_heading_words) {
-			std::transform(hadw.begin(),hadw.end(),hadw.begin(),::toupper); // TODO: This toupper Does NOT work for UTF-8 codepoints > 7F
-			std::transform(jew.begin(),jew.end(),jew.begin(),::toupper);
-			// std::cout << "\ncompare " << std::quoted(hadw) << " with " << std::quoted(jew);
-			if (hadw == jew) {
-				result = true;
-			}
+	auto s1_words = utf_ignore_to_upper(tokenize::splits(s1));
+	auto s2_words = utf_ignore_to_upper(tokenize::splits(s2));
+	for (int i=0; (i < s1_words.size()) and !result;++i) {
+		for (int j=0; (j < s2_words.size()) and !result;++j) {
+			result = (s1_words[i] == s2_words[j]);
 		}
 	}
 	return result;
+}
+
+bool first_in_second_case_insensitive(std::string const& s1, std::string const& s2) {
+	auto upper_s1 = utf_ignore_to_upper(s1);
+	auto upper_s2 = utf_ignore_to_upper(s2);
+	return (upper_s2.find(upper_s1) != std::string::npos);
 }
 
 namespace SRU {
@@ -4353,10 +4372,10 @@ std::string prompt_line(PromptState const& prompt_state) {
 		case PromptState::CounterTransactionsAggregateOption: {		
 			prompt << "had:aggregate:counter:";
 		} break;
-		case PromptState::NetVATAccountInput: {
+		case PromptState::GrossAccountInput: {
 			prompt << "had:aggregate:counter:gross";
 		} break;
-		case PromptState::GrossAccountInput: {
+		case PromptState::NetVATAccountInput: {
 			prompt << "had:aggregate:counter:net+vat";
 		} break;
 		case PromptState::JEAggregateOptionIndex: {
@@ -5161,7 +5180,7 @@ public:
 					else {
 						// Assume match to account name
 						for (auto const& [account_no,am] : model->sie["current"].account_metas()) {
-							if (strings_share_tokens(ast[1],am.name) or (am.name.find(ast[1]) != std::string::npos)) {
+							if (first_in_second_case_insensitive(ast[1],am.name)) {
 								prompt << "\n  " << account_no << " " << std::quoted(am.name);
 								if (am.sru_code) prompt << " SRU:" << *am.sru_code;
 							}
@@ -5291,7 +5310,17 @@ public:
 			else {
 				std::cout << "\nAct on words";
 				// Assume word based input
-				if (model->prompt_state == PromptState::EnterHA) {
+				if ((model->prompt_state == PromptState::NetVATAccountInput) or (model->prompt_state == PromptState::GrossAccountInput))  {
+					// Assume the user has enterd text to search for suitable accounts
+					// Assume match to account name
+					for (auto const& [account_no,am] : model->sie["current"].account_metas()) {
+						if (first_in_second_case_insensitive(ast[1],am.name)) {
+							prompt << "\n  " << account_no << " " << std::quoted(am.name);
+							if (am.sru_code) prompt << " SRU:" << *am.sru_code;
+						}
+					}
+				}
+				else if (model->prompt_state == PromptState::EnterHA) {
 					if (auto had_iter = model->selected_had()) {
 						auto& had = *(*had_iter);
 						if (!had.current_candidate) std::cerr << "\nNo had.current_candidate";
