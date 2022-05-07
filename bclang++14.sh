05495e644ffd4e62b8758543e1c269af34711b1a
kjelloh <>
2022-05-07 18:26:40 +0200

    Now compliles with clang++14 in Linux

--- bclang++14.sh
+++ bclang++14.sh
@@ -1,2 +1,2 @@
 clear
-/usr/lib/llvm-14/bin/clang++ -std=c++20 --sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -ferror-limit=5 src/main.cpp
+/usr/lib/llvm-14/bin/clang++ -std=c++20 -stdlib=libc++ -ferror-limit=5 src/main.cpp
