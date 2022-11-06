b9a7b1df696e188f1c8bd46f34308a812bd8e414
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-11-06 23:51:16 +0100

    Now compiles with std::ranges on macOS with clang 15 (both Brew installed and macPorts installed)

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,27 @@ float const VERSION = 0.5;
 #include <functional>
 #include <set>
 
+#include <ranges>
+ 
+void test_ranges_support()
+{
+    auto const ints = {0,1,2,3,4,5};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+ 
+    // "pipe" syntax of composing the views:
+    for (int i : ints | std::views::filter(even) | std::ranges::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+ 
+    std::cout << '\n';
+ 
+    // a traditional "functional" composing syntax:
+    for (int i : std::views::transform(std::views::filter(ints, even), square)) {
+        std::cout << i << ' ';
+    }
+}
+
 
 // Scratch comments to "remember" what configuration for VSCode that does "work"
 
@@ -9322,6 +9343,9 @@ private:
 
 int main(int argc, char *argv[])
 {
+	if (true) {
+		test_ranges_support();
+	}
 	if (false) {
 		// Log current locale and test charachter encoding.
 		// TODO: Activate to adjust for cross platform handling 
