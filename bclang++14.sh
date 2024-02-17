a3d13a5c84ed76ac61f4cd7d1fa2789f2e52925b
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2024-02-17 13:06:26 +0100

    Moved SKV SRU definition file to resources and removed shell build scripts

--- bclang++14.sh
+++
@@ -1,2 +0,0 @@
-clear
-/usr/lib/llvm-14/bin/clang++ -std=c++20 -stdlib=libc++ -ferror-limit=5 src/main.cpp
--- bg++11.sh
+++
@@ -1,2 +0,0 @@
-clear
-/usr/local/bin/g++-11 -std=c++20 --sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -fmax-errors=5 src/main.cpp
