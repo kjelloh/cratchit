4e28427f0f51462b64225f8a932029ac5719e3d2
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-02-02 20:58:07 +0100

    Now uses Elm architecture in runtime.hpp

--- src/cratchit.cpp
+++ src/cratchit.cpp
@@ -22,6 +22,13 @@ namespace first {
     std::shared_ptr<MsgImpl> pimpl;   
   };
 
+  std::optional<Msg> onKey(Event event) {
+    if (event.contains("Key")) {
+      return Msg{std::make_shared<NCursesKey>(std::stoi(event["Key"]))};
+    }
+    return std::nullopt;
+  }
+
   using Cmd = std::function<std::optional<Msg>()>;
 
   std::optional<Msg> Nop() {
@@ -113,9 +120,10 @@ namespace first {
     return {model,Nop}; // Return updated model
   }
 
-  pugi::xml_document view(const Model &model) {
+  Html_Msg<Msg> view(const Model &model) {
     // Create a new pugi document
-    pugi::xml_document doc;
+    Html_Msg<Msg> ui{};
+    auto& doc = ui.doc;
 
     // Note: HTML doc may be tested for validity at:
     // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro
@@ -149,136 +157,13 @@ namespace first {
     input.append_attribute("id") = "command";
     input.append_attribute("name") = "command";
 
-    return doc;
+    ui.event_handlers["OnKey"] = onKey;
+    return ui;
   }
-
-  void render_section(WINDOW *win, const std::string &text, int start_y,
-                      int max_lines) {
-    int line_count = 0;
-    size_t pos = 0;
-    while (pos < text.size() && line_count < max_lines) {
-      size_t next_line_pos = text.find('\n', pos);
-      if (next_line_pos == std::string::npos) {
-        next_line_pos = text.size();
-      }
-
-      std::string line = text.substr(pos, next_line_pos - pos);
-      mvwprintw(win, start_y + line_count, 1, "%s",
-                line.c_str()); // Render inside window
-
-      line_count++;
-      pos = next_line_pos + 1; // Move to the next line
-    }
-    wrefresh(win); // Refresh to show changes in the window
-  }
-
-  void render_prompt(WINDOW *win, const pugi::xml_node &prompt_node) {
-    // User prompt at the bottom of the screen (in the last row)
-    const std::string prompt_text =
-        prompt_node.child("label").text().as_string();
-    mvwprintw(win, 1, 1, "%s", prompt_text.c_str());
-    wmove(win, 1, prompt_text.size() + 1); // Move cursor after the prompt
-    wrefresh(win);                         // Refresh to show prompt
-  }
-
-  // Renders doc as HTML to ncurses screen
-  // Note: HTML doc semantics may be tested at:
-  // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro
-  void render(const pugi::xml_document &doc) {
-    int screen_height, screen_width;
-    getmaxyx(stdscr, screen_height, screen_width); // Get screen dimensions
-
-    // Create a window for the app screen area (excluding the last row for the
-    // prompt)
-    int app_screen_height =
-        screen_height - 1; // Excluding the bottom row for the prompt
-    int section_height =
-        app_screen_height /
-        3; // Divide the remaining screen height into three sections
-
-    // Create windows for the app screen sections
-    WINDOW *top_win = newwin(section_height, screen_width, 0, 0);
-    box(top_win, 0, 0); // Draw border around the top section
-    wrefresh(top_win);  // Refresh to show the window
-
-    WINDOW *middle_win =
-        newwin(section_height, screen_width, section_height, 0);
-    box(middle_win, 0, 0); // Draw border around the middle section
-    wrefresh(middle_win);  // Refresh to show the window
-
-    WINDOW *bottom_win =
-        newwin(section_height, screen_width, 2 * section_height, 0);
-    box(bottom_win, 0, 0); // Draw border around the bottom section
-    wrefresh(bottom_win);  // Refresh to show the window
-
-    // Parse the HTML-like structure
-    pugi::xml_node html = doc.child("html");
-    pugi::xml_node body = html.child("body");
-
-    int current_y = 1; // Start from row 1 to leave space for the top border
-    int num_divs = 0;
-
-    // Loop through divs directly and render them in sections
-    for (auto const &div : body.children("div")) {
-      // Render the content of the div inside the windows
-      const std::string text = div.text().as_string();
-      const int max_lines = section_height - 2; // Accounting for borders
-
-      if (div.attribute("class").as_string() == std::string("content")) {
-        if (num_divs == 0) {
-          render_section(top_win, text, current_y, max_lines);
-        } else if (num_divs == 1) {
-          render_section(middle_win, text, current_y, max_lines);
-        }
-      } else if (div.attribute("class").as_string() ==
-                 std::string("user-prompt")) {
-        render_prompt(bottom_win, div);
-      }
-
-      num_divs++;
-    }
-  }
-
-  class Ncurses {
-  public:
-    Ncurses() {
-      initscr();
-      cbreak();
-      noecho();
-      keypad(stdscr, TRUE);
-    }
-    ~Ncurses() {
-      endwin(); // End ncurses mode
-    }
-  };
 
   int main(int argc, char *argv[]) {
-#ifdef __APPLE__
-    // Quick fix to make ncurses find the terminal setting on macOS
-    setenv("TERMINFO", "/usr/share/terminfo", 1);
-#endif
-
-    int ch = ' '; // Variable to store the user's input
-    // init ncurses
-    Ncurses ncurses{};
-
-    std::queue<Msg> q{};
-    auto [model,cmd] = init();
-    if (auto msg = cmd()) q.push(*msg);
-    // Main loop
-    int loop_count{};
-    while (model.stack.size() > 0 and
-          not(model.user_input.size() == 1 and ch == 'q')) {
-      pugi::xml_document doc = view(model);
-      render(doc);
-      ch = getch();
-      q.push(Msg{std::make_shared<NCursesKey>(ch)});
-      auto msg = q.front(); q.pop();
-      auto const& [m,cmd] = update(model,msg);
-      model = m;
-      if (auto msg = cmd()) q.push(*msg);
-    }
-    return (ch == '-') ? 1 : 0;
+    Runtime<Model,Msg,Cmd> app(init,view,update);
+    return app.run(argc,argv);
   }
 } // namespace first
 
