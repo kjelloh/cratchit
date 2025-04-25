0690667bd12a08958004147451808fe63a44586e
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-04-25 17:05:21 +0200

    Now always calls MsgImpl base calss constructor

--- src/msg.cpp
+++ src/msg.cpp
@@ -6,11 +6,11 @@ namespace first {
 
   // ----------------------------------
   PushStateMsg::PushStateMsg(State const &parent, State const &state)
-      : m_parent{parent}, m_state{state} {}
+      : m_parent{parent}, m_state{state}, MsgImpl{} {}
 
   // ----------------------------------
   PoppedStateCargoMsg::PoppedStateCargoMsg(State const &top, std::string cargo)
-      : m_top{top}, m_cargo{cargo} {}
+      : m_top{top}, m_cargo{cargo}, MsgImpl{} {}
 
   // ----------------------------------
   Msg const QUIT_MSG{std::make_shared<Quit>()};
