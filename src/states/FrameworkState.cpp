427d6a028a6195118aa81ba554086a4187684930
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-07-13 19:11:51 +0200

    WorkspareState now provides user option in create_update_options only

--- src/states/FrameworkState.cpp
+++ src/states/FrameworkState.cpp
@@ -10,13 +10,27 @@ namespace first {
         ,m_root_path{root_path} { 
     this->m_ux.push_back(m_root_path.string());
 
-    this->add_cmd_option('0', WorkspaceState::cmd_option_from(m_root_path));
+    // Moved to create_update_options()
+    // this->add_cmd_option('0', WorkspaceState::cmd_option_from(m_root_path));
   }
 
   StateImpl::UpdateOptions FrameworkState::create_update_options() const {
     StateImpl::UpdateOptions result{};
-    // TODO: Refactor add_update_option in constructor to update options here
-    // TODO: Refactor add_cmd_option in constructor to update options here
+    
+    // Convert WorkspaceState::cmd_option_from to update option
+    auto caption = m_root_path.filename().string();
+    if (caption.empty()) {
+      caption = m_root_path.string(); // Use full path if filename is empty
+    }
+    
+    result.add('0', {caption, [root_path = m_root_path]() -> StateUpdateResult {
+      return {std::nullopt, [root_path]() -> std::optional<Msg> {
+        auto workspace_ux = StateImpl::UX{"Workspace UX"};
+        State new_state = std::make_shared<WorkspaceState>(workspace_ux, root_path);
+        return std::make_shared<PushStateMsg>(new_state);
+      }};
+    }});
+    
     return result;
   }
 
