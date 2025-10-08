7e63a5692bd19b552303778417b9c9efa11f3a86
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-10-08 15:17:55 +0200

    Added a note about tagged amount hash function ptentially having to handle 'transient' meta-data that does not change 'the value'

--- src/fiscal/amount/TaggedAmountFramework.hpp
+++ src/fiscal/amount/TaggedAmountFramework.hpp
@@ -86,6 +86,9 @@ namespace std {
       hash_combine(result, static_cast<unsigned>(yyyymmdd.day()));
       hash_combine(result, ta.cents_amount());
       for (auto const &[key, value] : ta.tags()) {
+        // TODO 20251008 - Consider if all tags really are members of the 'value'
+        //                 or if we need tags that are 'transient' as meta-data we may change
+        //                 without actually 'mutating the value'?
         hash_combine(result, key);
         hash_combine(result, value);
       }
