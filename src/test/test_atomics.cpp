77dec30f2168c0530d134a01159282f374827612
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-10-28 16:36:18 +0100

    Added comment how to make EmptyStageThreeTest pass

--- src/test/test_atomics.cpp
+++ src/test/test_atomics.cpp
@@ -1038,7 +1038,9 @@ R"(#GEN 20251026
 
         TEST_F(SIEEnvsMergeFixture,EmptyStageThreeTest) {
           logger::scope_logger log_raii{logger::development_trace,"TEST_F(SIEEnvsMergeFixture,EmptyStageThreeTest)"};
-          SIEEnvironment merged{};
+          SIEEnvironment merged{}; 
+          // TODO: Prohibit SIEEnvironment without valid fiscal year
+          //       for now stage fails because provided sie does not match dates accepted by merged...
           auto stage_result = merged.stage(fixture_three_entries_env);
           ASSERT_TRUE(fixture_three_entries_env.journals_entry_count() == 3);
           ASSERT_TRUE(stage_result.size() == 3);
