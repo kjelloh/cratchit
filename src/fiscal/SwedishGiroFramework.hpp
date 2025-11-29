ddbb6f8451acf88c2265582d32b4e81acc30dcf6
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-11-29 17:13:07 +0100

    Introduced SwedishGiroFramework unit (empty)

---
+++ src/fiscal/SwedishGiroFramework.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace giro {
+
+  namespace PG {
+
+    // Swedish 'Plusgirot' id processing
+
+  } // PG
+
+  namespace BG {
+
+    // Swedish 'Bankgirot' id processing
+
+  } // BG
+
+} // giro
