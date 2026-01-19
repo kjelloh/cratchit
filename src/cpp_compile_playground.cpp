0b67b79cab148aba14d64afcd6ef7bacd818d0ba
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-01-19 17:02:29 +0100

    REPLICATED compiler error for first::to_string blocking global to_string (DOES NOT COMPILE)

--- src/cpp_compile_playground.cpp
+++ src/cpp_compile_playground.cpp
@@ -1,5 +1,49 @@
 #include <string>
 
+// Trying to reproduce to_string compileation error
+// See docs/thinking/thinking.md 'How can I make all free function to_string overloads to be found by the compiler?'
+
+// 1. A global namespace to_string
+// 2. A first::to_string blocks global namespace to_string to bee seen from within namespace first
+
+namespace first {
+
+  struct Date {};
+  struct Amount {};
+} // first
+
+namespace second{
+  struct Date {};
+  struct Amount {};
+} // second 
+
+// Use first
+using Date = first::Date;
+using Amount = first::Amount;
+// [1] global to_string
+std::string to_string(Date) { return "second::date"; }
+std::string to_string(Amount) { return "second::amount"; }
+
+namespace first {
+
+  // [2] This to_string makes global to_string NOT SEEN
+  struct DateRange {}; 
+  std::string to_string(DateRange) { return "range"; } // Newly added
+
+  void test(Date d, Amount a) {
+      to_string(d);  // ERROR: unqualified lookup finds first::to_string(DateRange), no viable function
+      to_string(a);  // ERROR: -"-
+  }
+
+}
+
+
+// Trying to reproduce to_string compileation error
+// See docs/thinking/thinking.md 'How can I make all free function to_string overloads to be found by the compiler?'
+
+// 20260119 - Why does code below compile while AccountStatementState.cpp requires 
+//            'using ::to_string' in namespace first to compile?
+
 namespace N1 {
   struct some_type {};
   std::string to_string(some_type const& x) {
