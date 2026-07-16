512706bd550b3a2dfd1fc6925f65bda56b3131b5
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-07-16 13:39:47 +0200

    Intrdouced scaffholding for log_to_file and captions for business,development_trace and design_insufficiency

--- src/second/log.cpp
+++ src/second/log.cpp
@@ -1,10 +1,18 @@
 #include "log.hpp"
 #include <print>
 
+namespace detail {
+  void log_to_file(std::string_view sv) {
+    std::print("\nSOME_TIME_STAMP:{}",sv);
+  }
+} // detail
+
 void log_business(std::string_view sv) {
+  detail::log_to_file(std::format("BUSINESS:'{}'",sv));
 }
 void log_development_trace(std::string_view sv) {
-  std::print("DEVELOPMENT_TRACE:{}",sv);
+  detail::log_to_file(std::format("DEVELOPMENT_TRACE:'{}'",sv));
 }
 void log_design_insufficiency(std::string_view sv) {
+  detail::log_to_file(std::format("DESIGN_INSUFFICIENCY:'{}'",sv));
 }
--- src/second/main.cpp
+++ src/second/main.cpp
@@ -4,6 +4,8 @@
 namespace second {
   int main(int argc, char *argv[]) {
     log_development_trace("Hello from second::main");
+    log_business("second::cratchit START");
+    log_design_insufficiency("This is a test");
     return 0;
   }
 } // second
