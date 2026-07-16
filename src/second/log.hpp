2a8b0f4c98a5c7f336a022d08889613136998251
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-07-16 14:16:40 +0200

    Introduced std::format-like logging functions and test calls

--- src/second/log.hpp
+++ src/second/log.hpp
@@ -1,6 +1,22 @@
 #pragma once
 #include <string_view>
+#include <format>
 
 void log_business(std::string_view sv);
 void log_development_trace(std::string_view sv);
 void log_design_insufficiency(std::string_view sv);
+
+template <typename... Args>
+void log_business(std::format_string<Args...> fmt, Args&&... args) {
+  log_business(std::format(fmt,std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void log_development_trace(std::format_string<Args...> fmt, Args&&... args) {
+  log_development_trace(std::format(fmt,std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void log_design_insufficiency(std::format_string<Args...> fmt, Args&&... args) {
+  log_design_insufficiency(std::format(fmt,std::forward<Args>(args)...));
+}
--- src/second/main.cpp
+++ src/second/main.cpp
@@ -3,9 +3,13 @@
 
 namespace second {
   int main(int argc, char *argv[]) {
-    log_development_trace("Hello from second::main");
     log_business("second::cratchit START");
+    log_development_trace("Hello from second::main");
     log_design_insufficiency("This is a test");
+
+    log_business("Test with formatting int value {}",2);
+    log_development_trace("Test with formatting int value {}",3);
+    log_design_insufficiency("Test with formatting int value {}",4);
     return 0;
   }
 } // second
