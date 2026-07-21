8b230746d86ff13a928a8ee634637cc05a4c78cb
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-07-21 13:53:08 +0200

    Introduced raylib core example (plain window with greeting text on white background)

--- src/second/main.cpp
+++ src/second/main.cpp
@@ -1,6 +1,8 @@
 #include "main.hpp"
 #include "log.hpp"
 
+#include "raylib.h"
+
 #include <print>
 #include <iostream>
 #include <format>
@@ -15,6 +17,41 @@ namespace second {
     log_business("Test with formatting int value {}",2);
     log_development_trace("Test with formatting int value {}",3);
     log_design_insufficiency("Test with formatting int value {}",4);
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0; 
   }
