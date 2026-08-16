7111f8d63bbf3a87a083d495374b6b753cef67c6
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-08-16 16:11:20 +0200

    Introduced test.zsh (a mirror of run.zsh but for cratchit_test and google test arguments)

---
+++ test.zsh
@@ -0,0 +1,73 @@
+#!/bin/zsh
+
+EXECUTABLE=cratchit_test
+ERROR_LIMIT=3
+# With SEQUENTIAL_BUILD=true we don't multitask build on several threads.
+# And on single therad we can stop on compilation error and easilly see what the first error is!
+SEQUENTIAL_BUILD=true
+
+# Read current preset from file, default to conan-debug if file doesn't exist
+if [[ -f "cmake_preset.txt" ]]; then
+    PRESET_NAME=$(cat cmake_preset.txt)
+    echo "test.zsh: Using preset from cmake_preset.txt: $PRESET_NAME"
+    
+    # Extract build type from preset name for workspace directory
+    case $PRESET_NAME in
+        "conan-debug") BUILD_TYPE="Debug" ;;
+        "conan-release") BUILD_TYPE="Release" ;;
+        "conan-relwithdebinfo") BUILD_TYPE="RelWithDebInfo" ;;
+        "conan-minsizerel") BUILD_TYPE="MinSizeRel" ;;
+        *) 
+            echo "test.zsh: Error: Unknown preset in cmake_preset.txt: $PRESET_NAME"
+            echo "test.zsh: Run './init_tool_chain.zsh [Debug|Release|RelWithDebInfo|MinSizeRel]' to fix"
+            exit 1
+            ;;
+    esac
+else
+    PRESET_NAME="conan-debug"
+    BUILD_TYPE="Debug"
+    echo "test.zsh: Warning: cmake_preset.txt not found, defaulting to conan-debug"
+    echo "test.zsh: Run './init_tool_chain.zsh [Debug|Release|RelWithDebInfo|MinSizeRel]' first to set preset"
+fi
+
+# Define the workspace directory
+WORKSPACE_DIR="workspace"
+
+echo "test.zsh: Running $BUILD_TYPE build with preset $PRESET_NAME..."
+
+# Create the workspace directory if it doesn't exist
+echo "test.zsh: Creating workspace directory..."
+mkdir -p "$WORKSPACE_DIR"
+
+# Configure with error limit, then build
+echo "test.zsh: Configuring with error limit..."
+cmake --preset $PRESET_NAME -DMAX_COMPILE_ERRORS=$ERROR_LIMIT
+
+echo "test.zsh: Building the project with cmake..."
+if [[ "$SEQUENTIAL_BUILD" == "true" ]]; then
+    echo "test.zsh: Using sequential build (SEQUENTIAL_BUILD=$SEQUENTIAL_BUILD)"
+    cmake --build --preset $PRESET_NAME --parallel 1
+else
+    echo "test.zsh: Using parallel build (SEQUENTIAL_BUILD=$SEQUENTIAL_BUILD)"
+    cmake --build --preset $PRESET_NAME
+fi
+
+# Check if the build was successful
+if [[ $? -ne 0 ]]; then
+    echo "test.zsh: Build failed. Exiting."
+    exit 1
+fi
+
+# Copy the built binary to the workspace directory (mirror)
+echo "test.zsh: Copying the '$EXECUTABLE' binary to the workspace directory..."
+cp "build/$BUILD_TYPE/src/second/tests/$EXECUTABLE" "$WORKSPACE_DIR/"
+# Copy the built resources subfolder to the workspace subfolder (mirror)
+echo "test.zsh: Copying resources..."
+mkdir -p "$WORKSPACE_DIR/resources"
+cp -R "build/$BUILD_TYPE/resources/" \
+      "$WORKSPACE_DIR/resources/"
+
+# Change to the workspace directory
+cd "$WORKSPACE_DIR"
+
+./$EXECUTABLE "$@"
